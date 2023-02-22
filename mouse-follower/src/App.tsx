099ec6865e260e50aeb223788124e4fd5b7ba97f
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-02-21 19:44:13 -0500

    feat: adding useEffect to handle event listener

--- mouse-follower/src/App.tsx
+++ mouse-follower/src/App.tsx
@@ -1,13 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface IPosition {
+  clientX: number
+  clientY: number
+}
+
+
+const FollowMouse = () => {
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState<{ x: number, y: number }>({ x: 0, y: 0 })
+
+  const handleMove = (event: MouseEvent) => {
+    const { clientX, clientY }: IPosition = event
+    setPosition({ x: clientX, y: clientY })
+    console.log('handle', { clientX, clientY })
+  }
+
+  useEffect(() => {
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // executed when the component is unmounted or when the dependencies change
+    return () => window.removeEventListener('pointermove', handleMove)
+  }, [enabled])
 
   return (
-    <div className="App">
-      <h1>new app</h1>
-    </div>
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          opacity: 0.8,
+          borderRadius: '50%',
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      />
+      <button
+        onClick={() => setEnabled(!enabled)}
+      >
+        {enabled ? 'Desactivar' : 'Activar'}
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
